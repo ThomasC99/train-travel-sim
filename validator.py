ee223c802563db3e20a0d9e12abb8bb62b7ad830
ThomasC99 <50059204+ThomasC99@users.noreply.github.com>
2024-03-03 12:23:10 -0500

    Created level validator

---
+++ validator.py
@@ -0,0 +1,28 @@
+import sys
+import json
+
+file_name = sys.argv[1]
+
+file = open(file_name, "r")
+data = json.loads(file.read())
+file.close()
+
+# Service validation
+for service in data["services"]:
+    for schedule_item in data["services"][service]:
+        for station in schedule_item.split(" - "):
+            if station not in data["stations"]:
+                print("Station '" + station + "' found in service '" + service + "' does not exist")
+
+# Station validation
+for station in data["stations"]:
+    for line in data["stations"][station]:
+        if line not in data["services"]:
+            print("Station '" + station + "' not found in services")
+        else:
+            found = False
+            for item in data["services"][line]:
+                if station in item:
+                    found = True
+            if not found:
+                print("Station '" + station + "' not found in service '" + line + "'")
